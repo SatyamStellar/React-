3d4f25152a0f909e80793410fba0a9357a21921c
Satyam Tomar <satyam321tomar@gmail.com>
2024-10-26 19:21:48 +0530

    Complete the modular button component

--- BrainWave(JsMastry)/src/App.jsx
+++ BrainWave(JsMastry)/src/App.jsx
@@ -1,14 +1,11 @@
 import ButtonGradiant from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-2xl text-white">Satyam Tomar</h1>
       <div className="pt-[4.75rem] lg:pt[5.25rem] overflow-hidden"></div>
-      <Button className="mt-10" herf="#login">
-        Something
-      </Button>
+      <Header />
       <ButtonGradiant />
     </>
   );
